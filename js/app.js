d373e05b47bab053cdc512ef62dcc01ddeec2eea
Jue Yang <jueyang22@gmail.com>
2012-04-25 18:01:38 -0400

    fixed flicker with help. need to fix buttons, restructure css, add another grid-like display for the photos. learning, son, learning

--- js/app.js
+++ js/app.js
@@ -1,97 +1,66 @@
 var mm = MM;
-var url = 'http://api.tiles.mapbox.com/v3/';
-var baseLayer = 'jue.map-9f0twc3h';
-var locLayer = ',jue.coasttowns';
+var m;
+var url = 'http://api.tiles.mapbox.com/v3/jue.map-9f0twc3h,jue.coasttowns';
 
-wax.tilejson(url + baseLayer + locLayer + '.jsonp',function(tilejson) {
-	tilejson.minzoom = 7;
-	tilejson.maxzoom = 16;
-	m = new mm.Map('map', new wax.mm.connector(tilejson),null, [
-		new mm.MouseHandler(),
-		new mm.TouchHandler()
-		]
-	);
-	m.setZoomRange(7, 16);
-	m.setCenterZoom(new mm.Location(43.565,-127.049),8);
-	wax.mm.zoomer(map,tilejson).appendTo(map.parent);
-	tilejson.attribution = '';
-
+wax.tilejson(url + '.jsonp',function(tilejson) {
+    	m = new mm.Map('map', 
+    	new wax.mm.connector(tilejson),null, [
+        new easey.DragHandler(),
+        new easey.TouchHandler(),
+        new easey.MouseWheelHandler(),
+        new easey.DoubleClickHandler()
+	    ]);
+    m.setZoomRange(7, 16);
+    m.setCenterZoom(new mm.Location(43.565,-127.049),8);
+    wax.mm.zoomer(m,tilejson).appendTo(m.parent);
     wax.mm.interaction()
-    	.map(m).tilejson(tilejson).on(wax.movetip().parent(m.parent).events())
-    		
-    // avoid tooltip overlap, not working yet
+    	.map(m)
+    	.tilejson(tilejson)
+    	.on(wax.movetip().parent(m.parent).events());
+    var extent = m.getExtent();
+    var bbox = [extent.west, extent.south, extent.east, extent.north].join(',');
+});
 
-    /*		{on: function(){
-    			if (zoomlevel == 10) {
-                    console.log(zoomlevel);
-                	$('.wax-tooltip').addClass('z10');  
-    			}
-    		}
-    	);	
-    */
-    	
-    //trying to turn on "photo layer" with callback and zoom
-    
-     /*m.addCallback("drawn", function (m){
-         var zoomlevel = m.getZoom();
-         	if (zoomlevel == 11){
-         		$('.pictures img').addClass('appear');
-         	}
-         	if (zoomlevel > 11){
-         		$('.pictures img').removeClass('appear');
-            }
-     });
-    */
-    
-	//refresh
 
-function refreshMap(baseLayer) {
-    wax.tilejson(url + baseLayer + locLayer + '.jsonp', function (tilejson) {
-          tilejson.minzoom = 6;
-          tilejson.maxzoom = 14;
-          m.setProvider(new wax.mm.connector(tilejson));
-          $('.wax-legends').remove();
-          wax.mm.legend(m, tilejson).appendTo(m.parent);
-          wax.mm.interaction(m,tilejson);
-      });
-}
 
-    //flickr
+//get photos from flickr, draw them on the screen (ie.Locationpoint) in follower.js
 
 window.jsonFlickrApi = function(rsp) {
-	var photos = rsp.photos.photo;
-	for (var i = 0; i < photos.length; i++) {
-    	var p = photos[i];
-    	var url = [ 'http://farm', p.farm, '.static.flickr.com/', p.server, '/', p.id, '_', p.secret, '_s.jpg' ].join('');    
-       	var html = "<" + "img src='" + url + "'" + ">"; // weird for 'eval', sorry
-       	var location = new mm.Location(p.latitude, p.longitude);
-       	var dimensions = new mm.Point(75, 75); //this gives dimension to div
-       	var f = new mm.Follower(m, location, html, dimensions);
-   	}
+    var photos = rsp.photos.photo;
+    for (var i = 0; i < photos.length; i++) {
+        var p = photos[i];
+        var url = [ 'http://farm', p.farm, '.static.flickr.com/', p.server, '/', p.id, '_', p.secret, '_s.jpg' ].join('');
+        var img = document.createElement('img');
+        img.src = url;    
+        // var html = "<" + "img src='" + url + "'" + ">"; // weird for 'eval', sorry
+        var location = new MM.Location(p.latitude, p.longitude);
+        var dimensions = new MM.Point(75, 75);
+        var f = new MM.Follower(m, location, img, dimensions);
+    }
 }
-	var extent = m.getExtent();
-	var bbox = [extent.west, extent.south, extent.east, extent.north].join(',');
-	var script = document.createElement('script');
-		script.src = 'http://api.flickr.com/services/rest/?'+
-        	       'method=flickr.photos.search&'+
-            	   'api_key=a96cbef093a8c0280d3aed4e0c004d4c&'+
-                   'tags=coast&'+
-                   'extras=geo&'+
-               	   'bbox=' + bbox +
-                   'has_geo=1&'+
-                   'format=json';
-		document.getElementsByTagName('head')[0].appendChild(script);
 
-});
+function flickr(){
+	var extent = m.getExtent();
+    var bbox = [extent.west, extent.south, extent.east, extent.north].join(',');
+	var script = document.createElement('script');
+	script.src = 'http://api.flickr.com/services/rest/?'+
+	               'method=flickr.photos.search&'+
+	               'api_key=a96cbef093a8c0280d3aed4e0c004d4c&'+
+	               'tags=coast&'+
+	               'bbox=' + bbox +
+	               '&extras=geo&'+
+	               'has_geo=1&'+
+	               'format=json';
+	document.getElementsByTagName('head')[0].appendChild(script);
+}
 	
-    
-    // easeIt
+// easeIt
 
 function easeIt(x, y, z, callback) {
     var options = {
         location: new mm.Location(x, y),
         zoom: z || 13,
-        ease: 'ease-in',
+        ease: 'ease',
         time: 2000
         };
     if (typeof callback === 'function') {
@@ -100,8 +69,23 @@ function easeIt(x, y, z, callback) {
         easey.slow(m, options);
 }
 
-   // Layer Selection
-   
+// layer expand and selection
+
+
+	//i have question!
+$('ul.layerswitch li a').hover(
+	function(){
+		if(!$(this).hasClass('active')){
+			$('.active').addClass('hover');
+		}
+	},
+	function(){
+		if(!$(this).hasClass('hover')){
+			$('.active').removeClass('hover');
+		}
+	}
+);
+
 $('ul.layerswitch li a').click(function (e) {
     e.preventDefault();
     if (!$(this).hasClass('active')) {
@@ -110,7 +94,7 @@ $('ul.layerswitch li a').click(function (e) {
         if (this.id == 'oregon') {
         	easeIt(43.565,-123.795,8);
         	$('.open').css('display','none');
-        	$('.intro').css('display','block');
+        	$('.flickr').css('display','block');
         	$('#c1').css('display','none');
         	$('.newport-story').css('display','none');
         	$('#c2').css('display','none');
@@ -122,7 +106,7 @@ $('ul.layerswitch li a').click(function (e) {
         if (this.id == 'newport') {
         	easeIt(44.6133,-124.0655,10);
         	$('.open').css('display','none');
-        	$('.intro').css('display','none');
+        	$('.flickr').css('display','none');
         	$('#c1').css('display','block');
         	$('.newport-story').css('display','block');
         	$('#c2').css('display','none');
@@ -133,7 +117,7 @@ $('ul.layerswitch li a').click(function (e) {
         if (this.id == 'bandon') {
    			easeIt(43.1431,-124.3432,10);
    			$('.open').css('display','none');
-   			$('.intro').css('display','none');
+   			$('.flickr').css('display','none');
    			$('#c1').css('display','none');
         	$('.newport-story').css('display','none');
    			$('#c2').css('display','block');
@@ -145,7 +129,7 @@ $('ul.layerswitch li a').click(function (e) {
         if (this.id == 'beach') {
    			easeIt(42.203847,-124.374956,10);
    			$('.open').css('display','none');
-   			$('.intro').css('display','none');
+   			$('.flickr').css('display','none');
    			$('#c1').css('display','none');
         	$('.newport-story').css('display','none');
         	$('#c2').css('display','none');
@@ -158,63 +142,29 @@ $('ul.layerswitch li a').click(function (e) {
     }
 });
 
-
-
-	// click for topbar and continuing stories
+// click for topbar and continuing stories
 	
-$('#cont1').click(function (e) {
+$('#proceed').click(function (e) {
     e.preventDefault();
+    $('.towns').css('display','block');
     $('ul.layerswitch li a').removeClass('active');
 	$('#newport').addClass('active');
     $('.open').fadeOut('4000');
+    $('#c1').css('display','block');
     $('.newport-story').css('display','block');
     $('#topbar').fadeIn('4000');
     $('#topbar').css('display','block');
     easeIt(44.6133,-124.0655,10);
 });
-    
-$('#cont2').click(function (e) {
-	e.preventDefault();
-	$('ul.layerswitch li a').removeClass('active');
-	$('#bandon').addClass('active');
-	$('#c1').css('display','none');
-    $('.newport-story').css('display','none');
-	$('#c2').css('display','block');
-    $('.bandon-story').css('display','block');
-    $('.intro').css('display','none');
-    easeIt(43.1431,-124.3432,10);
-});
 
-$('#cont3').click(function (e) {
-	e.preventDefault();
-	$('ul.layerswitch li a').removeClass('active');
-	$('#beach').addClass('active');
-	$('#c2').css('display','none');
-    $('.bandon-story').css('display','none');
-	$('#c3').css('display','block');
-    $('.beach-story').css('display','block');
-    easeIt(42.203847,-124.374956,10);
-});
-
-$('#cont4').click(function (e) {
-	e.preventDefault();
-	$('ul.layerswitch li a').removeClass('active');
-	$('#oregon').addClass('active');
-	$('#c3').css('display','none');
-    $('.beach-story').css('display','none');
-    $('.intro').fadeIn('4000');
-    $('#intro-click').fadeIn('4000');
-    easeIt(43.565,-123.795,8);
-});
-
-
-    // return to opening page
+// return to opening page
     
 $('.title a').click(function (e) {
 	e.preventDefault();
 	$('#topbar').fadeOut('8000');
 	$('.open').fadeIn('8000');
-   	$('.intro').css('display','none');
+	$('.towns').css('display','none');
+   	$('#flickr').css('display','none');
     $('#c1').css('display','none');
   	$('.newport-story').css('display','none');
     $('#c2').css('display','none');
@@ -224,21 +174,21 @@ $('.title a').click(function (e) {
    	easeIt(43.565,-127.049,10);
 });
 
-	// turn pictures on (how to get it into a loop?)
+// turn pictures on
 	
-$('#intro-click').click(function (e){
+$('#flickr-click').click(function (e){
 	e.preventDefault();
+	flickr();
 	$(this).addClass('change');
-	$('.pictures img').addClass('appear');
-	/*	
-	$(this).hover(
+	//$('.pictures img').addClass('appear');
+	/*$(this).hover(
 		function() {
 			$(this).addClass('again');
 		},
 		function() {
 			$(this).removeClass('again');
 		}
-	);*/
+	);
 	$(this).click(function (e){
 		if ($('.pictures img').hasClass('appear')){
 			$('.pictures img').removeClass('appear');
@@ -248,7 +198,7 @@ $('#intro-click').click(function (e){
 			$('.pictures img').addClass('appear');
 			$(this).removeClass('again');
 		}
-	});
+	});*/
 });
 
 
