a109af93f7657425643ddc8a247782169ba19658
jueyang <jueyang22@gmail.com>
2014-08-05 03:49:58 -0400

    in search of organization

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-var app = app || {};
-
-app.towns = {
+$(function(){
+var towns = {
 "type": "FeatureCollection",
 "features": [
   {
@@ -54,33 +53,85 @@ app.towns = {
 ]
 }
 
-// app.jsonFlickrApi = function(rsp) {
-//   var photos = rsp.photos.photo;
-//   for (var i = 0; i < photos.length; i++) {
-//       var p = photos[i];
-//       var url = [ 'http://farm', p.farm, '.static.flickr.com/', p.server, '/', p.id, '_', p.secret, '_s.jpg' ].join('');
-//       var img = document.createElement('img');
-//       img.src = url;    
-//       // var html = "<" + "img src='" + url + "'" + ">"; // weird for 'eval', sorry
-//   }
-// }
+L.mapbox.accessToken = 'pk.eyJ1IjoianVlIiwiYSI6InFsakR2UEkifQ.GSsNWZF7HVlLqwdhWuM2gA';
 
-// app.flickr = function(){
-//   var extent = map.getBounds();
-//   var bbox = [extent.west, extent.south, extent.east, extent.north].join(',');
-//   var script = document.createElement('script');
-//   script.src = 'https://api.flickr.com/services/rest/?'+
-//                'method=flickr.photos.search&'+
-//                'api_key=a96cbef093a8c0280d3aed4e0c004d4c&'+
-//                'tags=coast&'+
-//                'bbox=' + bbox +
-//                '&extras=geo&'+
-//                'has_geo=1&'+
-//                'format=json';
-//   document.getElementsByTagName('head')[0].appendChild(script);
-// };
+var map = L.mapbox.map('map', 'jue.map-9f0twc3h',{
+    minZoom:6,
+    maxZoom:9,
+    zoomControl: false
+}).setView([43.5, -128], 7);
+
+var dot = L.icon({
+    iconUrl: 'images/blurred-dot.png',
+    iconSize: [100,100]
+});
+
+var layerGroup = L.layerGroup(), //store a copy of layers to be used for DOM/map interaction
+    activeStory = false;
+
+var townLayers = L.geoJson(towns,{
+    pointToLayer: function(feature,latlng){
+        return L.marker(latlng, {icon: dot});
+    },
+    onEachFeature: function(feature,layer){
+        var title = '<h1>' + feature.properties.name + '</h1>';
+            paragraph = '<p>' + feature.properties.story + '</p>',
+            all = '<div class="story">' + title + paragraph + '</div>';
+
+        // popup of both town name and story - for click
+        feature.properties.all = L.popup({
+            closeButton: false,
+            keepInView: true,
+            offset: [-300,100]
+        })
+        .setContent(all)
+        .setLatLng(layer.getLatLng());
+
+        layer.on('click',function(){
+            map.openPopup(feature.properties.all);
+            activeStory = feature.properties.id;
+        }).addTo(layerGroup);
+    }
+}).addTo(map);
+
+map.on('popupopen',function(){
+    $('.open').removeClass('fadeIn');
+    $('.open').addClass('fadeOut');
+    $('.fader').addClass('blink');
+});
 
 
+$('.fader').bind('click',crossFade);
+$('.looper').bind('click',toNextStory);
+
+function crossFade(){
+    $('.open').toggleClass('fadeIn fadeOut');
+    $('.story').toggleClass('fadeIn',false)
+        .toggleClass('fadeOut',true);
+    $('.fader').toggleClass('blink');
+}
+
+function toNextStory(){
+
+  var next;
+  if (!activeStory || activeStory >= layerGroup.getLayers().length){
+    next = 1;
+  } else if (activeStory < layerGroup.getLayers().length){
+    next = activeStory + 1;
+  }
+
+  layerGroup.eachLayer(function(layer) {
+    if (layer.feature.properties.id === next) {
+      nextStory = layer.feature.properties.all;
+      map.setView(layer.getLatLng(), layer.feature.properties.zoom);
+      map.openPopup(nextStory);
+
+      paragraphOpen = true;
+      activeStory = next;
+      }
+    });
+  }
+});
 	
   
 
